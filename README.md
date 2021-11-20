2611733972dc76a8a9675bad40125530c0fafeed
belmichellemx <91795424+belmichellemx@users.noreply.github.com>
2021-11-19 19:57:08 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# collisionAlgorithm
-Algorithm to detect collision between two objects
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -1,50 +1,31 @@
-var fixedRect, movingRect;
-var gameObject1,gameObject2,gameObject3,gameObject4;
+const Engine= Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var object;
 
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
 
-  gameObject1 = createSprite(100,100,50,50);
-  gameObject1.shapeColor = "pink";
-  gameObject2 = createSprite(200,100,50,50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300,100,50,50);
-  gameObject3.shapeColor = "gray";
-  gameObject4 = createSprite(400,100,50,50);
-  gameObject4.shapeColor = "yellow";
+  var object_options={
+    isStatic: true
+  }
+
+
+  object = Bodies.rectangle(200,100,50,50,object_options);
+  World.add(world,object);
+
+  console.log(object.position.x);
+  console.log(object.position.y);
+
 }
 
 function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-
-  if (isTouching(movingRect,gameObject2)){
-    movingRect.shapeColor = "blue";
-    gameObject2.shapeColor = "blue";
-  }
-  else{
-    movingRect.shapeColor = "green";
-    gameObject2.shapeColor = "green";
-  }
-  
-  drawSprites();
-}
-
-function isTouching(object1, object2){
-  if (object1.x - object2.x < object2.width/2 + object1.width/2
-    && object2.x - object1.x < object2.width/2 + object1.width/2
-    && object1.y - object2.y < object2.height/2 + object1.height/2
-    && object2.y - object1.y < object2.height/2 + object1.height/2) {
-    return true;
-}
-else {
-    return false;
-}
+  background(0);  
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(object.position.x,object.position.y,50,50);
 }
